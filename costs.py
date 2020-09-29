8e662fede65fab2073e098ff643fddb1d11decea
Gasser Hassan <gasserh0@gmail.com>
2020-09-29 19:35:19 +0200

    Exercise 2 functions added

--- costs.py
+++ costs.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 """Function used to compute the loss."""
+import numpy as np
 
-def compute_loss(y, tx, w):
-    """Calculate the loss.
+def compute_MSE_loss(y, tx, w):
+    fw = tx@w.T
+    e = y - fw
+    cost = e**2
+    loss = np.mean(cost)/2
+    return loss
 
-    You can calculate the loss using mse or mae.
-    """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE / MAE
-    # ***************************************************
-    raise NotImplementedError
+
+def compute_MAE_loss(y, tx, w):
+    f = tx@w.T
+    cost = np.abs(y-f)
+    loss = np.mean(cost)/2
+    return loss
