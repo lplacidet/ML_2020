8008be8ce5c5f2c12750328e0ac1469d53de62d2
Louise <louise.placidet@gmail.com>
2020-10-10 17:01:24 +0200

    Projet 1 formated

---
+++ implementations.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+
+return w, loss
+
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+
+return w, loss
+
+
+
+def least_squares(y, tx):
+
+return w, loss
+
+
+def ridge_regression(y, tx, lambda_):
+
+return w, loss
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+
+return w, loss
+
+def reg_logistic_regression(y, tx, lambda_, intial_w, max_iters, gamma):
+
+return w, loss
